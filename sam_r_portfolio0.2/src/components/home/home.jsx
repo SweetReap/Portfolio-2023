2aeea0f8dec6ab731bfa7baf9576dc233fd028cd
Samantha Reape <54642592+SweetPotats@users.noreply.github.com>
2023-02-24 14:30:05 -0500

    02-24-23 Added Style To Skills + More Edits

--- sam_r_portfolio0.2/src/components/home/home.jsx
+++ sam_r_portfolio0.2/src/components/home/home.jsx
@@ -1,64 +1,101 @@
 import Spline_Sam from '../../assets/3D/spline_sam';
+import {SiAdobeillustrator, SiCsharp, SiProcessingfoundation, SiAdobephotoshop, SiAdobeaftereffects} from 'react-icons/si';
 import './styles.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export default function Home(){
-    return(
-        <div className="Container">
 
-            <div className='intro-container'>
+    useEffect(() =>{
+        AOS.init({
+            duration: 1000,
+            mirror:true,
+            anchorPlacement: 'top-bottom'
+        });
+    }, []);
+
+    return(
+        
+        <div className="Container">
+            <div className='intro-container' data-aos="fade-down">
                 <div className="intro">
                     <p className="small-txt-title">Hi! I'm...</p>
                     <div className='name-logo'>
-                      <p>samantha reape</p>
+                      <img src='./images/final-logo/SVG/my-name-light.svg' width={'200px'}/>
                    </div>
                     <p className="role-title">Game Developer | Web Developer | Digital Artist</p>
-                    <hr/>
+                    {/* <hr/> */}
                     <p className='about-me-short'>
                         I'm currently a student in NYC who strives to improve the user experience and create solutions to modern problems. 
-                        <br/> Along with that, I have many interests in game development, web design and UI/UX design.
+                        <br/> Along with that, I have a huge passion for game development, web design, and UI/UX design.
                         </p>
                     <button className='about-me-more'>| Explore More |</button>
                 </div>
 
-                <div className="image-right-side">
+                <div className="image-right-side" data-aos='fade-up' data-aos-delay='100'>
                     <div className='container-spline-head'>
                         <Spline_Sam />
                     </div>
                 </div>
             </div>
 
-           <div className='skills-list'>
-                <h1>Skills</h1> {/* add some logos */}
-                <ul>
-                    <h3>Tools</h3>
-                    <li>
-                        <a>React.JS</a>
-                    </li>
-                    <li>
-                        <a>Three.JS</a>
-                    </li>
-                    <li>
-                        <a>Unity</a>
-                    </li>
-                    <li>
-                        <a>Adobe Photoshop</a>
-                    </li>
-                    <li>
-                        <a>Adobe Illustrator</a>
-                    </li>
-
-                    <h3>Languages</h3>
-                    <li>
-                        <a>Java</a>
-                    </li>
-                    <li>
-                        <a>JavaScript</a>
-                    </li>
-                    <li>
-                        <a>C#</a>
-                    </li>
-                </ul>
-           </div>
+            <div className='skills-container' data-aos="fade-down">
+            <div className='skill-spline-sam'>
+                   <p>hello</p>
+            </div>
+            <div className='skills-list' data-aos="flip-down">
+                        <h1>Skills</h1> {/* add some logos */}
+                        
+                        <ul data-aos="fade-down">
+                            <h3>Tools</h3>
+                            <div className='skills'>
+                                <li>
+                                    <i className="fa-brands fa-react"></i>
+                                    <a>React.JS</a>
+                                </li>
+                                
+                                <li>
+                                    <i className="fa-brands fa-unity"></i>
+                                    <a>Unity</a>
+                                </li>
+                                <li>
+                                    <SiAdobephotoshop className='icon-react'/>
+                                    <a>Adobe Photoshop</a>
+                                </li>
+                                <li>
+                                    <SiAdobeillustrator className='icon-react'/>
+                                    <a>Adobe Illustrator</a>
+                                </li>
+                                <li>
+                                    <SiAdobeaftereffects className='icon-react'/>
+                                    <a>Adobe After Effects</a>
+                                </li>
+                            </div>
+                            
+                            <h3>Languages</h3>
+                            <div className='skills'>
+                                <li>
+                                    <i className="fa-brands fa-java"></i>
+                                    <a>Java</a>
+                                </li>
+                                <li>
+                                    <i className="fa-brands fa-square-js"></i>
+                                    <a>JavaScript</a>
+                                </li>
+                                <li>
+                                    <SiCsharp className='icon-react' />
+                                    <a>C-Sharp</a>
+                                </li>
+                                <li>
+                                    <SiProcessingfoundation className='icon-react'/>
+                                    <a>Processing 4</a>
+                                </li>
+                            </div>
+                        </ul>
+            </div>
+               
+            </div>
         </div>
         
     );
